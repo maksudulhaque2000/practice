a0272e6f58c1ac239ab70fad8a23cf90dd90a7f4
Haksudul_Haque <smmaksudulhaque2000@gmail.com>
2025-09-16 23:34:32 +0600

    Added HTML and Css File and connect eatch other

--- TypeScript/dist/index.js
+++ TypeScript/dist/index.js
@@ -5,45 +5,39 @@
 //         course
 //     }
 // }
-
 // const student1 = addCourseToStudent ({name: 'Maksudul Haque', email: 'maksudulhaque@gmail.com', devType: 'Pro'})
 // const student2 = addCourseToStudent ({name: 'Mahmudul Haque', email: 'mahmudulhaque@gmail.com', devType: 'Noob', merried: false})
 // console.log(student1, student2);
-
 // class Student {
 //     name: string
 //     email: string
 //     devType: string
 //     merried: boolean
-
 //     constructor(name: string, email: string, devType: string, merried: boolean) {
 //         this.name = name
 //         this.email = email
 //         this.devType = devType
 //         this.merried = merried
 //     }
-
 //     displayInfo() : void {
 //         console.log(`Student Name: ${this.name}, Email: ${this.email}, Dev Type: ${this.devType}, Merried: ${this.merried}`);
 //     }
 // }
 // const student3 = new Student("Maksudul Haque", "smmaksudulhaque2000@gmail.com", "Pro", false);
 // student3.displayInfo()
-
 // class Teacher extends Student {
 //     designation: string
-    
 //     constructor(name: string, email: string, devType: string, merried: boolean, designation: string) {
 //         super(name, email, devType, merried)
 //         this.designation = designation
 //     }
-
 //     displayInfo() : void {
 //         super.displayInfo()
 //         console.log(`Designation: ${this.designation}`);
 //     }
 // }
-
-const userForm = document.querySelector('#userForm') as HTMLFormElement;
+const userForm = document.querySelector('#userForm');
 console.log(userForm);
 console.log(userForm.userEmail);
+export {};
+//# sourceMappingURL=index.js.map
