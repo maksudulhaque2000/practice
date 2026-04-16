d48e0516a368fb9900b4d6dd64478305f4956f0d
Maksudul-Haque <smmaksudulhaque2000@gmail.com>
2026-04-16 22:24:30 +0600

    Implement data fetching and display from API in HTML and JavaScript

--- script.js
+++ script.js
@@ -1,5 +1,14 @@
 const fetchData = () => {
-  fetch("https://jsonplaceholder.typicode.com/todos")
+  const url = "https://jsonplaceholder.typicode.com/todos";
+  fetch(url)
     .then((response) => response.json())
-    .then((json) => console.log(json));
+    .then((json) => eachData(json));
+};
+const eachData = (json) => {
+  const dataList = document.getElementById("data-list");
+  json.forEach((element) => {
+    const listItem = document.createElement("li");
+    listItem.textContent = element.title;
+    dataList.appendChild(listItem);
+  });
 };
