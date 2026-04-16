b038cc2b063b185c9088573d08ce457c360f4e57
Maksudul-Haque <smmaksudulhaque2000@gmail.com>
2026-04-16 22:55:53 +0600

    Refactor data fetching functions and update HTML structure for unordered and ordered lists

--- script.js
+++ script.js
@@ -1,14 +1,29 @@
-const fetchData = () => {
+const fetchDataOl = () => {
   const url = "https://jsonplaceholder.typicode.com/todos";
   fetch(url)
     .then((response) => response.json())
-    .then((json) => eachData(json));
+    .then((json) => eachDataOl(json));
 };
-const eachData = (json) => {
-  const dataList = document.getElementById("data-list");
+const eachDataOl = (json) => {
+  const dataListUl = document.getElementById("data-list-ol");
   json.forEach((element) => {
-    const listItem = document.createElement("li");
-    listItem.textContent = element.title;
-    dataList.appendChild(listItem);
+    const li = document.createElement("li");
+    li.innerText = element.title;
+    dataListUl.appendChild(li);
+  });
+};
+
+const fetchDataUl = () => {
+  const url = "https://jsonplaceholder.typicode.com/todos";
+  fetch(url)
+    .then((response) => response.json())
+    .then((json) => eachDataUl(json));
+};
+const eachDataUl = (json) => {
+  const dataListOl = document.getElementById("data-list-ul");
+  json.forEach((element) => {
+    const li = document.createElement("li");
+    li.innerText = element.title;
+    dataListOl.appendChild(li);
   });
 };
