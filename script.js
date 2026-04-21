6e4a5d9476d163984b7252d486299b55e15470ef
Maksudul-Haque <smmaksudulhaque2000@gmail.com>
2026-04-21 20:52:17 +0600

    Refactor HTML structure and improve code formatting in JavaScript and CSS files

--- script.js
+++ script.js
@@ -1,18 +1,18 @@
-  const fetchTodos = () => {
+const fetchTodos = () => {
   fetch("https://jsonplaceholder.typicode.com/todos")
-  .then((response) => response.json())
-  .then((data) => showPosts(data))
-}
+    .then((response) => response.json())
+    .then((data) => showPosts(data));
+};
 const showPosts = (data) => {
-  const todosContainer = document.getElementById("todos")
+  const todosContainer = document.getElementById("todos");
   data.forEach((todo) => {
-    const todoCard = document.createElement("div")
+    const todoCard = document.createElement("div");
     todoCard.innerHTML = `
       <div class="todo-card">
         <h2>${todo.title}</h2>
         <h4>${todo.completed}</h4>
       </div>
-    `
-  todosContainer.appendChild(todoCard)
+    `;
+    todosContainer.appendChild(todoCard);
   });
-}
+};
